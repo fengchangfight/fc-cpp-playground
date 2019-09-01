47e0556a16abb6068318597ebdf61237344e93f5
fengchang <fengchang_fight@163.com>
2019-09-01 15:55:18 +0800

    add std swap example

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <gflags/gflags.h>
 #include <boost/tokenizer.hpp>
 #include <boost/shared_ptr.hpp>
+#include <openssl/opensslv.h>
 
 using std::cout;
 using std::endl;
@@ -11,6 +12,28 @@ using std::string;
 using std::vector;
 
 int val = 1;
+
+void vectorplay()
+{
+     // 移除重复元素
+     std::vector<int> v{1, 2, 3, 1, 2, 3, 3, 4, 5, 4, 5, 6, 7};
+     std::sort(v.begin(), v.end()); // 1 1 2 2 3 3 3 4 4 5 5 6 7
+     auto last = std::unique(v.begin(), v.end());
+     // v 现在保有 {1 2 3 4 5 6 7 x x x x x x} ，其中 x 不确定
+     v.erase(last, v.end());
+     for (int i : v)
+          std::cout << i << " ";
+     std::cout << "\n";
+}
+
+void swapplay()
+{
+     int x = 10, y = 20;
+     cout << "before x:" << x << ",y:" << y << endl;
+     std::swap(x, y);
+
+     cout << "after x:" << x << ",y:" << y << endl;
+}
 
 class implementation
 {
@@ -28,6 +51,7 @@ void boostshareptrtest()
      std::cout << "The Sample now has " << sp2.use_count() << " references\n";
 
      sp1.reset();
+     cout << sp1.use_count() << endl;
      std::cout << "After Reset sp1. The Sample now has " << sp2.use_count() << " references\n";
 
      sp2.reset();
@@ -36,6 +60,6 @@ void boostshareptrtest()
 
 int main(int argc, char *argv[])
 {
-     boostshareptrtest();
+     swapplay();
      return 0;
 }
